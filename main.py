8ae03925355e2199b104dcd7c60d6ee1927fcc5b
Hawk <30607615+haawwkeye@users.noreply.github.com>
2022-02-25 23:32:31 +0000

    add bad apple stuff

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("RatatouilleEnding","RatatouilleEndingVideo.mp4",.1,.1,10)
+    encoder.encode("BadApple","[HD] Touhou - Bad Apple!! [ＰＶ] (Shadow Art)-360p.mp4",.1,.1,10)
 
 if __name__== "__main__":
     main()
