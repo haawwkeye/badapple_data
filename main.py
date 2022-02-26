5a571a0546c79ffdb6f318b03ef0788c9ff55036
haawwkeye <connorbishop53@gmail.com>
2022-02-25 20:56:58 -0500

    Add new format

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("BadApple","[HD] Touhou - Bad Apple!! [ＰＶ] (Shadow Art)-360p.mp4",1,1,10)
+    encoder.encode("BadApple","[HD] Touhou - Bad Apple!! [ＰＶ] (Shadow Art)-360p.mp4",0.5,0.4,96)
 
 if __name__== "__main__":
     main()
